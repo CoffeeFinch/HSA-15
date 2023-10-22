726395d19a46368ab5411f81f3d31e3b844cc34b
Will Gainsborough <wgainsborough37@gmail.com>
2023-10-22 15:02:18 -0400

    Setup Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Header />
+    <Audio />
+    <Branding />
+    <Coding />
+    <Illustration />
+    <ProjectPlanning />
+    <UXUI />
+    <Video />
+  </div>
+</template>
+
+<script setup>
+import Audio from '../components/Audio.vue'
+import Branding from '../components/Branding.vue'
+import Coding from '../components/Coding.vue'
+import Header from '../components/Header.vue'
+import Illustration from '../components/Illustration.vue'
+import ProjectPlanning from '../components/Project Planning.vue'
+import UXUI from '../components/UXUI.vue'
+import Video from '../components/Video.vue'
+</script>
